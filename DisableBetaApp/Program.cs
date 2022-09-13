a25b732e9e08a0d7c0999d90e5a6db841fe373be
TFilteredC <thomthom1@live.co.uk>
2022-09-13 16:47:56 +0100

    Add project files.

---
+++ DisableBetaApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace DisableBetaApp
+{
+    internal class Program
+    {
+        static void FindAndLaunchRoblox(string[] args, bool playGame)
+        {
+            string launchargs;
+            if (playGame == true) {
+                launchargs = args[0]
+                .Replace("+LaunchExp:InApp", "+LaunchExp:InBrowser");
+            } else {
+                launchargs = "--play";
+            }
+
+            string LocalAppDataEnv = System.Environment.GetEnvironmentVariable("LOCALAPPDATA");
+            string[] RobloxExecutable = Directory.GetFiles(LocalAppDataEnv + "\\Roblox\\Versions", "RobloxPlayerLauncher.exe", SearchOption.AllDirectories);
+            string Exec = RobloxExecutable[0];
+
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = Exec,
+                Arguments = launchargs,
+                UseShellExecute = true
+            };
+
+            Process.Start(startInfo); 
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Roblox...");
+            FindAndLaunchRoblox(args, args.Length == 1);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+i hate net 6
+
+To use, download the latest Release and override the `roblox-player` protocol in your browser.
+
+*Pull requests are welcome on trying to do this automatically since it gets overriden by the Roblox Launcher*
